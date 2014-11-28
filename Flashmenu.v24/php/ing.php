7ed428c953b1dcaa9b1104f4aa66a23d17b67035
daniel18df <daniel18.df@gmail.com>
2014-11-28 19:59:38 -0300

    version 1.24

---
+++ Flashmenu.v24/php/ing.php
@@ -0,0 +1,66 @@
+<?php
+
+$var1=$_GET['var1'];
+
+if(isset($_POST['submit'])){
+  // include db connect class
+    require_once __DIR__ . '/db_connect.php';
+    // connecting to db
+    $db = new DB_CONNECT();
+    $nombre = $_POST['nombre'];
+    $descripcion = $_POST['descripcion'];
+    $precio = $_POST['precio'];
+  $query = "INSERT INTO platos (Platos_nombre, Platos_descripcion, Platos_precio, Restaurant_idRestaurant) VALUES ('$nombre', '$descripcion', '$precio', '$var1')";
+     if (!mysql_query($query))
+         {
+         die('Error: ' . mysql_error());
+         echo "Error al crear el plato." . "<br />";
+         }
+         else{
+         echo "<br />" . "<h2>" . "Plato Creado Exitosamente!" . "</h2>";
+         echo "<h4>" . "Nombre plato: " . $_POST['nombre'] . "</h4>";
+         }
+         //}
+         mysql_close($conexion);
+     }
+        ?>
+        
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<LINK REL="SHORTCUT ICON" HREF="images/favicon.png"> 
+    <title>Registrar Usuario</title>
+    <meta charset = "utf-8">
+</head>
+    <body background="images/bg.jpg">
+        <br>
+        <br>
+        <center><span style="font-size:55px;"> <h1> <font color="red" face = "Berlin Sans FB"> FLASHMENU</font> </h1> </span></center>
+        </br>
+        </br>
+    <header>
+         <center><h1><font color= "white"> Ingresar platos: </font></h1> </center>
+    </header>  
+    <form  name="procesar-usuario" enctype="multipart/form-data" action="ing.php?var1=<?php echo $var1?>">
+<center>
+    <hr/>
+    <!--Nombre plato-->
+     <label for="nombre"><font color= "white">Nombre plato:</font></label>
+     <input type="text" name="nombre" maxlength="30" />
+    <br/><br/>
+
+     <!--descripcion plato--> 
+     <label for="descripcion"><font color= "white">Descripcion:</font></label>
+     <input type="text" name="descripcion" maxlength="30" />
+     <br/><br/>
+    <!--precio plato-->
+     <label for="precio"><font color= "white">Precio:</font></label>
+     <input type="text" name="precio" maxlength="16" />
+    <br/><br/>
+<input type="submit" name="submit" value="Ingresar..." formmethod="post" />
+<input type="reset" name="clear" value="Borrar"/>
+    </center>
+    </form>
+<hr />
+    </body>
+</html>
---
+++ Flashmenu.v24/php/perfil3.php
@@ -0,0 +1,79 @@
+<?php
+$var1=$_GET['var1'];
+/*
+ * Following code will list all the products
+ */
+
+// include db connect class
+/*
+require_once __DIR__ . '/db_connect.php';
+
+// connecting to db
+$db = new DB_CONNECT();
+
+$result = mysql_query("SELECT *FROM platos WHERE Restaurant_idRestaurant = '$var1'") or die(mysql_error());
+
+if (mysql_num_rows($result) > 0) {
+   
+    while ($row = mysql_fetch_array($result)) {
+       
+        $idPlatos = $row["idPlatos"];
+        $Platos_nombre = $row["Platos_nombre"];
+        $Platos_descripcion = $row["Platos_descripcion"];
+        $Platos_precio = $row["Platos_precio"];
+      }*/
+?>
+
+
+<!DOCTYPE html>
+    <html lang="en">
+ 
+<head>
+    <LINK REL="SHORTCUT ICON" HREF="images/favicon.png"> 
+ <title>Check Login</title>
+ <meta charset = "utf8" />
+    </head>
+
+
+         <body background="images/bg.jpg">
+
+        <br>
+        <br>
+        <center><span style="font-size:55px;"> <h1> <font color="red" face = "Berlin Sans FB"> FLASHMENU</font> </h1> </span></center>
+        </br>
+        </br>
+
+ <center><h1><font color= "white"> Perfil restaurant</font></h1> </center>
+
+    
+    <center>
+    <?php    
+    echo "<a href='ing.php?var1=$var1'><button>Ingresar platos</button></a>"
+
+    ?>
+    <a href='ver.php'><button>Ver platos</button></a>
+    </center>
+
+
+<!--
+<?php
+/*
+} 
+else {
+    // no products found
+    $response["success"] = 0;
+    $response["message"] = "No restaurant found";
+
+    // echo no users JSON
+    echo json_encode($response);
+}
+
+*/
+?>
+-->
+
+
+
+
+</body>
+</html>
